b1a5664f27ca02fb845a9bfcce6a861cc839cd39
Paulo Dias <dias.paulo@gmail.com>
2015-04-11 15:57:12 +0100

    Update SamlFactory.php

--- DependencyInjection/Security/Factory/SamlFactory.php
+++ DependencyInjection/Security/Factory/SamlFactory.php
@@ -10,6 +10,7 @@ namespace PDias\SamlBundle\DependencyInjection\Security\Factory;
 use Symfony\Component\DependencyInjection\ContainerBuilder,
     Symfony\Component\DependencyInjection\Reference,
     Symfony\Component\DependencyInjection\DefinitionDecorator,
+    Symfony\Component\Config\Definition\Builder\NodeDefinition,
     Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\AbstractFactory;
 
 /**
@@ -17,19 +18,27 @@ use Symfony\Component\DependencyInjection\ContainerBuilder,
  */
 class SamlFactory extends AbstractFactory
 {
-    public function __construct() 
-    { 
-        $this->addOption('create_user_if_not_exists', false); 
-    } 
-    
+
     public function getPosition()
     {
         return 'pre_auth';
+        //return 'http';
+        //return 'form';
     }
 
     public function getKey()
     {
         return 'saml';
+    }
+    
+    public function addConfiguration(NodeDefinition $node) 
+    { 
+        parent::addConfiguration($node);
+
+        $builder = $node->children();
+        $builder
+            ->booleanNode('direct_entry')->defaultTrue()->end()
+        ;
     }
     
     protected function getListenerId()
@@ -40,19 +49,10 @@ class SamlFactory extends AbstractFactory
     protected function createAuthProvider(ContainerBuilder $container, $id, $config, $userProviderId)
     {
         $authProviderId = 'security.authentication.provider.saml.'.$id;
-        $definition =$container
+        $container
             ->setDefinition($authProviderId, new DefinitionDecorator('saml.security.authentication.provider'))
             ->replaceArgument(0, new Reference($userProviderId));
         
-        // with user provider 
-        if (isset($config['provider'])) { 
-            $definition 
-                ->addArgument(new Reference($userProviderId)) 
-                ->addArgument(new Reference('security.user_checker')) 
-                ->addArgument($config['create_user_if_not_exists']) 
-            ; 
-        } 
-
         return $authProviderId;
     }
 
@@ -60,20 +60,42 @@ class SamlFactory extends AbstractFactory
     {
         $listenerId = $this->getListenerId();
         $listener = new DefinitionDecorator($listenerId);
+        $listener->replaceArgument(7, $config);
         $listenerId .= '.'.$id;
         $container->setDefinition($listenerId, $listener);
-		
+        
+        //Logout listener/handler
+        $this->createLogoutHandler($container, $id, $config);
+        
+        return $listenerId;
+    }
+    
+    /*protected function createEntryPoint($container, $id, $config, $defaultEntryPointId)
+    {
+        $entryPointId = 'saml.security.authentication.entry_point.'.$id;
+        $container 
+            ->setDefinition($entryPointId, new DefinitionDecorator('saml.security.authentication.entry_point'))
+            ->replaceArgument(2, $config)
+        ;
+
+        // set options to container for use by other classes
+        $container->setParameter('saml.options.'.$id, $config);
+
+        return $entryPointId;
+    }*/
+    
+    protected function createLogoutHandler($container, $id, $config)
+    {
         //Logout listener
         if($container->hasDefinition('security.logout_listener.'.$id)) {
             $logoutListener = $container->getDefinition('security.logout_listener.'.$id);
             $samlListenerId = 'security.logout.handler.saml';
 
             //Add logout handler
-            $container->setDefinition($samlListenerId, new DefinitionDecorator('saml.security.http.logout'));
+            $container
+                ->setDefinition($samlListenerId, new DefinitionDecorator('saml.security.http.logout'))
+                ->replaceArgument(2, array_intersect_key($config, $this->options));
             $logoutListener->addMethodCall('addHandler', array(new Reference($samlListenerId)));
         }
-
-        return $listenerId;
     }
-    
 }
