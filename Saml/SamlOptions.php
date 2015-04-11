033f89a4642acae4a583e7dc279605b5483a284f
Paulo Dias <dias.paulo@gmail.com>
2015-04-11 16:27:03 +0100

    Create SamlOptions.php

---
+++ Saml/SamlOptions.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the SamlBundle.
+ *
+ * (c) Paulo Dias <dias.paulo@gmail.com>
+ *
+ */
+namespace PDias\SamlBundle\Saml;
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+/**
+ * Handles the options of firewall
+ *
+ * @package    SamlBundle
+ * @subpackage Saml
+ * @author     Paulo Dias <dias.paulo@gmail.com>
+ */
+class SamlOptions
+{
+    protected $options;
+
+    public function __construct(array $options = array())
+    {
+        $this->options = new ParameterBag($options);
+    }
+    
+    public function getOptions()
+    {
+        return $this->options;
+    }
+    
+    public function getOption($optionId)
+    {
+        return $this->options->get($path);
+    }
+}
