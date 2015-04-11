d150c3e3f04def6640e45de3d3ce572ccf9daf9b
Paulo Dias <dias.paulo@gmail.com>
2015-04-11 16:29:51 +0100

    Update SamlListener.php

--- Security/Firewall/SamlListener.php
+++ Security/Firewall/SamlListener.php
@@ -7,78 +7,131 @@
  */
 namespace PDias\SamlBundle\Security\Firewall;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\Security\Http\Firewall\ListenerInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\SecurityContextInterface;
-use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\Security\Http\SecurityEvents;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Response,
+    Symfony\Component\HttpFoundation\RedirectResponse,
+    Symfony\Component\HttpKernel\Event\GetResponseEvent,
+    Symfony\Component\Security\Http\Firewall\ListenerInterface,
+    Symfony\Component\Security\Core\Exception\AuthenticationException,
+    Symfony\Component\Security\Core\SecurityContextInterface,
+    Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface,
+    Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface,
+    Symfony\Component\Security\Core\Authentication\Token\TokenInterface,
+    Symfony\Component\Security\Http\Event\InteractiveLoginEvent,
+    Symfony\Component\Security\Http\SecurityEvents,
+    Symfony\Component\EventDispatcher\EventDispatcherInterface,
+    Symfony\Component\HttpFoundation\ParameterBag;
 
 use PDias\SamlBundle\Security\Authentication\Token\SamlUserToken;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\HttpUtils;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use PDias\SamlBundle\Saml\SamlAuth;
 
 /**
  * @author: Paulo Dias <dias.paulo@gmail.com>
  */
 class SamlListener implements ListenerInterface
 {
+    protected $tokenStorage;
     protected $securityContext;
     protected $authenticationManager;
-    protected $serviceprovider;
-    protected $authenticationField;
-    protected $dispatcher;
-
-    public function __construct(SecurityContextInterface $securityContext, AuthenticationManagerInterface $authenticationManager, $serviceprovider, $authenticationField, EventDispatcherInterface $dispatcher = null)
+    protected $eventDispatcher;
+    protected $samlAuth;
+    protected $httpUtils;
+    protected $logger;
+    protected $options;
+    
+    public function __construct(
+            TokenStorageInterface $tokenStorage,
+            SecurityContextInterface $securityContext,
+            AuthenticationManagerInterface $authenticationManager,
+            HttpUtils $httpUtils,
+            EventDispatcherInterface $eventDispatcher = null,
+            SamlAuth $samlAuth,
+            LoggerInterface $logger = null,
+            array $options = array())
     {
+        $this->tokenStorage = $tokenStorage;
         $this->securityContext = $securityContext;
         $this->authenticationManager = $authenticationManager;
-        $this->dispatcher = $dispatcher;
-        $this->serviceprovider = $serviceprovider;
-        $this->authenticationField = $authenticationField;
+        $this->httpUtils = $httpUtils;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->samlAuth = $samlAuth;
+        $this->logger = $logger;
+        $this->options = $options;
     }
 
     public function handle(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-
-        $auth = new \SimpleSAML_Auth_Simple($this->serviceprovider); 
-        $auth->requireAuth(); 
-        $attributes = $auth->getAttributes();
-
-        $token = new SamlUserToken();
-
-        if(array_key_exists($this->authenticationField, $attributes)) {
-            $token->setUser($attributes[$this->authenticationField][0]);
-        } else {
-            throw new \InvalidArgumentException(sprintf('Your provider must return attribute "%s".', $this->authenticationField));
-        }
-
+        
         try {
-            $authToken = $this->authenticationManager->authenticate($token);
-			
+            $samlToken = new SamlUserToken();
+            $samlToken->setDirectEntry($this->options['direct_entry']);
+
+            $authToken = $this->authenticationManager->authenticate($samlToken);
             if ($authToken instanceof TokenInterface ) {
-                $this->securityContext->setToken($authToken);
-				
-                if (null !== $this->dispatcher) {
-                    $loginEvent = new InteractiveLoginEvent($request, $authToken);
-                    $this->dispatcher->dispatch(SecurityEvents::INTERACTIVE_LOGIN, $loginEvent);
-                }
-				
-                return;
+                $this->onSuccess($request, $authToken);
+                return $authToken;
             } else if ($authToken instanceof Response) {
                 return $event->setResponse($authToken);
             }
-        } catch (AuthenticationException $e) {
-            $response = new Response();
-            $response->setStatusCode(403);
-            $event->setResponse($response);
+        } catch (\Exception $e) {
+
+            $this->requestSaml($request);
+
+            $token = $this->tokenStorage->getToken();
+            if ($token instanceof SamlUserToken/* && $this->providerKey === $token->getProviderKey()*/) {
+                 $this->tokenStorage->setToken(null);
+            }
+            return;
+            
+            //throw new AuthenticationException('The Saml user could not be retrieved from the session.');
+        }
+        
+        // By default deny authorization
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_FORBIDDEN);
+        $event->setResponse($response);
+    }
+    
+    private function onSuccess(Request $request, TokenInterface $token)
+    {
+        if (null !== $this->logger) {
+            $this->logger->info(sprintf('User "%s" has been authenticated successfully', $token->getUsername()));
         }
 
-        $response = new Response();
-        $response->setStatusCode(403);
-        $event->setResponse($response);
+        $this->tokenStorage->setToken($token);
+        //$this->securityContext->setToken($token);
+
+        $session = $request->getSession();
+        $session->remove(Security::AUTHENTICATION_ERROR);
+        $session->remove(Security::LAST_USERNAME);
+
+        if (null !== $this->eventDispatcher) {
+            $loginEvent = new InteractiveLoginEvent($request, $token);
+            $this->eventDispatcher->dispatch(SecurityEvents::INTERACTIVE_LOGIN, $loginEvent);
+        }
+    }
+    
+    private function requestSaml(Request $request)
+    {
+        if($this->options['direct_entry'] || $this->httpUtils->checkRequestPath($request, $this->options['check_path']))
+        {
+            $this->samlAuth->setLoginReturn($this->getReturnUrl($request));
+            $this->samlAuth->requireAuth();
+        }
+    }
+    
+    private function getReturnUrl(Request $request)
+    {
+        if($this->options['always_use_default_target_path'] && isset($this->options['default_target_path'])) {
+            return $this->httpUtils->generateUri($request, $this->options['default_target_path']);
+        }
+        
+        //return $this->httpUtils->generateUri($request, $this->options['login_return']);
+        return $this->httpUtils->generateUri($request, '/');
     }
 }
